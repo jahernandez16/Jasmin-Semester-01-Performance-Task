58a3f2e35aa1956de324030b7cf05ba0fda8f3b2
Jasmin <jahernandez16@s.sfusd.edu>
2023-12-20 11:31:11 -0800

    collisions setup

--- player.gd
+++ player.gd
@@ -2,24 +2,23 @@ extends Area2D
 
 var speed = 400
 
-func _ready():
-	pass 
-
-
 func _process(delta):
 	var velocity = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_down"):
-		velocity += 1
+		velocity.y += 1
 	if Input.is_action_pressed("move_left"):
-		velocity -= 1	
+		velocity.x -= 1	
 	if Input.is_action_pressed("move_up"):
-		velocity -= 1 
+		velocity.y -= 1 
 	if Input.is_action_pressed("move_right"):
-		velocity += 1	
+		velocity.x += 1	
 	
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		position += velocity * delta
-		
+	
+
+func _on_body_entered(body):
+	hide()
 	
